7afc3a863d5c0d0c88b2263ac3d4a2c92b474ca7
Olivier Pi <olivier@opierre.fr>
2024-09-03 18:10:15 +0200

    fix: project migration

--- src/lib/types/resumeTypes.ts
+++ src/lib/types/resumeTypes.ts
@@ -1,49 +1,17 @@
+import { PersonalInformation, Experience, Skill, Education, Interest, Project } from '@prisma/client';
+
 export type ResumeData = {
-  personalInformation: {
-    id: number
-    name: string
-    title: string
-    location: string
-    phone: string
-    email: string
-    age: number
-    drivingLicense: string
-    languages: Record<string, string>
+  personalInformation: Omit<PersonalInformation, 'id' | 'createdAt' | 'updatedAt'> & {
+    languages: Record<string, string>;
+  };
+  summary: string;
+  skills: Record<string, Array<Omit<Skill, 'id' | 'createdAt' | 'updatedAt' | 'projects' | 'ProjectSkill'>>>;
+  experiences: Omit<Experience, 'createdAt' | 'updatedAt'>[];
+  education: Omit<Education, 'createdAt' | 'updatedAt'>[];
+  interests: Omit<Interest, 'createdAt' | 'updatedAt'>[];
+  projects: Array<
+    Omit<Project, 'createdAt' | 'updatedAt' | 'skills' | 'ProjectSkill'> & {
+    skills: string[];
   }
-  summary: string
-  skills: Record<string, Array<{ id: number; name: string; icon: string }>>
-  experiences: Array<{
-    id?: number
-    role: string
-    company: string
-    city: string
-    dates: string
-    description?: string
-    logo: string
-    tasks: string[]
-    startDate: string
-    endDate: string
-  }>
-  education: Array<{
-    id: number
-    degree: string
-    institution: string
-    dates: string
-    details?: string
-  }>
-  interests: Array<{
-    id: number
-    type: string
-    description: string
-    icon: string
-  }>
-  references: Array<{
-    id: number
-    name: string
-    company: string
-    role: string
-    description: string
-    image?: string
-    link?: string
-  }>
-}
+  >;
+};
