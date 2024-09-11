4a2d1d30b05fb3a6ba9bbf8ab7647fe10f49ebeb
Olivier Pi <olivier@opierre.fr>
2024-09-11 18:19:37 +0200

    fix: styles home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,39 @@ import Experience from '@/components/Experiences/Experiences';
 import Skill from '@/components/Competences/Competences';
 import {ProfileInfos} from "@/components/ProfileInfos/ProfileInfos";
 import {getExperiences, getSkills} from '@/lib/fetchData';
+import { SkillsSummary } from '@/lib/types/SkillsTypes';
 import styles from './ui/home.module.scss';
 
 export default async function HomePage () {
     const experiences = await getExperiences();
     const skills = await getSkills();
 
+  const skillCategories = {
+    Web: ['frontEnd', 'backEnd', 'databases', 'workflow', 'api'],
+    Projet: ['projectManage', 'event', 'tools', 'communication', ],
+  };
+
+  const categorizedSkills = {
+    Web: skills.filter((s: SkillsSummary) => skillCategories.Web.includes(s.type)),
+    Projet: skills.filter((s: SkillsSummary) => skillCategories.Projet.includes(s.type)),
+  };
+
   return (
     <main className={styles.containerHome}>
-        <ProfileInfos/>
+      <ProfileInfos />
       <section className={styles.skillsSection}>
-        <h2>Compétences</h2>
-            <Skill skill={skills} />
+        <h2>COMPÉTENCES</h2>
+        <h3>WEB</h3>
+        <div className={styles.skillsContainer}>
+          <Skill title="Web" skill={categorizedSkills.Web} />
+        </div>
+        <h3>PROJETS</h3>
+        <div className={styles.skillsContainer}>
+          <Skill title="Projet" skill={categorizedSkills.Projet} />
+        </div>
       </section>
       <section>
-        <h2>Expériences</h2>
+        <h2>EXPÉRIENCES</h2>
         <Experience experiences={experiences} />
       </section>
     </main>
