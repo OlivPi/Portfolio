921b80ebab1bece97183f3f32b6a88501ea38e4d
Olivier Pi <olivier@opierre.fr>
2024-09-17 21:15:33 +0200

    fix: style title header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,15 @@ interface PersonalInformationProps {
     personalInformation: PersonalInformationSummary[];
 }
 export default function Header({personalInformation}: PersonalInformationProps) {
+
+  console.log(personalInformation[0].title);
   return (
     <header className="pt-6 pb-24">
       <div className={'flex justify-between pb-24'}>
         <SiteTitle/>
         <Menu />
       </div>
-            <h2 className={'text-xl'}>{personalInformation[0].title}</h2>
+            <h2 className={'text-xl flex flex-col'}>{personalInformation[0].title}</h2>
     </header>
   )
 }
